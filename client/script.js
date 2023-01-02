856d7bdeeadf94ff07cc3024bda4e1527a19508d
Buzaition <42010650.Abuzaid@acu.edu.eg>
2023-01-02 16:41:19 +0200

    update URL

--- client/script.js
+++ client/script.js
@@ -62,7 +62,7 @@ function chatStripe(isAi, value, uniqueId) {
     )
 }
 
-const handleSubmit = async (e) => {
+const handleSubmit = async(e) => {
     e.preventDefault()
 
     const data = new FormData(form)
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    const response = await fetch('https://buzaition.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
